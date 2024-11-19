a74ae9e8edf64e5058f7af3eecf7c5bb0ca63c46
Bharath6730 <thanigaivelbharath@gmail.com>
2024-11-19 21:13:59 +0530

    modify for http

--- build/flutter_service_worker.js
+++ build/flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "37f7db088f499b6916f8b2522456d991",
-"/": "37f7db088f499b6916f8b2522456d991",
-"main.dart.js": "b341a5410787ef7dc70ad63be1fa585d",
+"index.html": "6cf46d5ade17038dddcede945d641c59",
+"/": "6cf46d5ade17038dddcede945d641c59",
+"main.dart.js": "9bb787579097744fac54688cfe855b0f",
 "manifest.json": "34d7a93a24a9edfdcbc76b9763f73a11",
 "version.json": "11e1a177d57c5da2bc6e3a6acc3010f4"};
 // The application shell files that are downloaded before a service worker can
